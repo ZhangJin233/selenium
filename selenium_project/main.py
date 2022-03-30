3414e5a0ff01b065e3756f98ffbbe5f80b62b6dc
ZhangJin233 <jane.zhangjin@outlook.com>
2022-03-30 22:43:18 +0800

    Add user testcase

--- selenium_project/main.py
+++ selenium_project/main.py
@@ -3,6 +3,9 @@ import testcases.testcase1
 import testcases.testcase2
 from util import util
 from testcases.basic.test_user_register import TestUserRegister
+from testcases.basic.test_user_login import TestUserLogin
+from testcases.basic.test_admin_login import TestAdminLogin
+
 
 
 
@@ -13,7 +16,14 @@ if __name__ == "__main__":
     # driver = webdriver.Chrome()
     # driver.get('http://localhost:8080/jpress/user/register')
     # util.get_coder(driver,'captchaimg')
-    case01 = TestUserRegister()
+    # case01 = TestUserLogin()
+    case01 = TestAdminLogin()
+
     # case01.test_register_code_error()
     # case01.test_not_fill_username()
-    case01.test_register_successful()
+    # case01.test_user_login_with_username_error()
+    # case01.test_not_fill_password()
+    # case01.test_login_success()
+    case01.test_fill_nothing_in_username()
+    case01.test_fill_nothing_in_password()
+    case01.test_login_with_error_code()
---
+++ selenium_project/testcases/basic/test_user_login.py
@@ -0,0 +1,62 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class TestUserLogin(object):
+
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get('http://localhost:8080/jpress/user/login')
+        self.driver.maximize_window()
+
+    def test_user_login_with_username_error(self):
+        username = 'username'
+        password = 'pwd'
+        expected = '用户名不正确。'
+        self.driver.find_element(By.NAME,'user').send_keys(username)
+        self.driver.find_element(By.NAME,'pwd').send_keys(password)
+        self.driver.find_element(By.CLASS_NAME,'btn').click()
+
+        WebDriverWait(self.driver, 5).until(EC.alert_is_present())
+        alert = self.driver.switch_to.alert
+
+        assert alert.text == expected
+        alert.accept()
+
+    def test_not_fill_password(self):
+        username = 'username'
+        expected = '密码不能为空'
+        self.driver.find_element(By.NAME,'user').clear()
+        self.driver.find_element(By.NAME,'pwd').clear()
+        self.driver.find_element(By.NAME,'user').send_keys(username)
+        self.driver.find_element(By.CLASS_NAME,'btn').click()
+        
+        WebDriverWait(self.driver, 5).until(EC.alert_is_present())
+        alert = self.driver.switch_to.alert
+
+        assert alert.text == expected
+        alert.accept()
+
+    def test_login_success(self):
+        username = 'test'
+        password = '123456'
+        expected = '用户中心'
+
+        self.driver.find_element(By.NAME,'user').clear()
+        self.driver.find_element(By.NAME,'pwd').clear()
+        self.driver.find_element(By.NAME,'user').send_keys(username)
+        self.driver.find_element(By.NAME,'pwd').send_keys(password)
+        self.driver.find_element(By.CLASS_NAME,'btn').click()
+
+
+        WebDriverWait(self.driver, 5).until(EC.title_is(expected))
+
+        assert self.driver.title == expected
+
+
+
+
+
+
+    
